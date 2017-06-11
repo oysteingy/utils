f903459583311d5a9b0f8269fd6fdef153474576
oysteingy <oystein@gyland.ninja>
2017-06-11 17:29:46 +0200

    update

--- network_to_wildcard_acl.py
+++ network_to_wildcard_acl.py
@@ -12,16 +12,18 @@ def convert():
         print("Incorrect IPv4 addr {}".format(err))
         sys.exit(1)
     #reuse netmask object as IPv4Address 
-    netmask = int.from_bytes(ipaddress.IPv4Address(netmask).packed, "big")
-    wildcard = netmask ^ int.from_bytes(ipaddress.IPv4Address((2**32)-1).packed,"big")
-    return (str(ipaddress.IPv4Address(wildcard)), str(ipaddress.IPv4Network(sys.argv[1]).network_address))
+    netmask = int(ipaddress.IPv4Address(netmask))
+    #wildcard = netmask XOR 255.255.255.255
+    wildcard = netmask ^ int(ipaddress.IPv4Address('255.255.255.255'))
+    #return wildcard and ip network 
+    return (str(ipaddress.IPv4Network(sys.argv[1]).network_address), str(ipaddress.IPv4Address(wildcard)))
 
 def main():
     if (len(sys.argv) != 2):
         print("{} network/mask ".format(sys.argv[0]))
         sys.exit(1)
     net_and_wc = (convert())
-    print("permit {} {} any".format(net_and_wc[1], net_and_wc[0]))
+    print("permit {} {} any".format(net_and_wc[0], net_and_wc[1]))
 
 if __name__ == '__main__':
     main()
